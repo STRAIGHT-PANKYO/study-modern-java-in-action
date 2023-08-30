aee84fdac725ad9ba98ede903e989c40222c31ac
gnlslfflffl <hyunjin.jo@nhnenter.com>
2023-08-30 19:06:31 +0900

    ch6 - 첫번째 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,34 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-    public static void main(String[] args) {
+ class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String numbers = br.readLine();
+
+        long count = Arrays.stream(numbers.split(" "))
+                           .map(Integer::parseInt)
+                           .filter(Main::isPrime)
+                           .count();
+
+        System.out.println(count);
+        br.close();
+    }
+
+    public static boolean isPrime(int candidate) {
+        if (candidate <= 1) {
+            return false;
+        }
+
+        int candidateRoot = (int) Math.sqrt(candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                        .noneMatch(i -> (candidate % i) == 0);
+
 
     }
 }
