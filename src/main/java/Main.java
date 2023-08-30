ed9dc6033928c7b05a53965e8af74108158a0112
gnlslfflffl <hyunjin.jo@nhnenter.com>
2023-08-30 19:45:13 +0900

    ch6 - 두번째 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,94 @@
+import static java.util.stream.Collector.Characteristics.IDENTITY_FINISH;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
- class Main {
+class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        String numbers = br.readLine();
+        String[] s = br.readLine().split(" ");
+        int m = Integer.parseInt(s[0]);
+        int n = Integer.parseInt(s[1]);
 
-        long count = Arrays.stream(numbers.split(" "))
-                           .map(Integer::parseInt)
-                           .filter(Main::isPrime)
-                           .count();
+        partitionPrimesWithCustomCollector(n)
+            .get(true)
+            .stream()
+            .filter(num -> num >= m)
+            .forEach(System.out::println);
 
-        System.out.println(count);
         br.close();
     }
 
-    public static boolean isPrime(int candidate) {
+    public static boolean isPrime(List<Integer> primes, int candidate) {
         if (candidate <= 1) {
             return false;
         }
 
         int candidateRoot = (int) Math.sqrt(candidate);
-        return IntStream.rangeClosed(2, candidateRoot)
-                        .noneMatch(i -> (candidate % i) == 0);
+        return primes.stream()
+                     .takeWhile(i -> i <= candidateRoot)
+                     .noneMatch(i -> candidate % i == 0);
 
+
+    }
+
+    public static Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                        .collect(new PrimeNumbersCollector());
+    }
+
+    static class PrimeNumbersCollector
+        implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
+
+        @Override
+        public Supplier<Map<Boolean, List<Integer>>> supplier() {
+            return () -> new HashMap<>() {{
+                put(true, new ArrayList<>());
+                put(false, new ArrayList<>());
+            }};
+        }
+
+        @Override
+        public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+            return (Map<Boolean, List<Integer>> acc, Integer candidate) -> {
+                acc.get(isPrime(acc.get(true), candidate))
+                   .add(candidate);
+            };
+        }
+
+        @Override
+        public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+            return (Map<Boolean, List<Integer>> map1, Map<Boolean, List<Integer>> map2) -> {
+                map1.get(true).addAll(map2.get(true));
+                map1.get(false).addAll(map2.get(false));
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+            return i -> i;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.unmodifiableSet(EnumSet.of(IDENTITY_FINISH));
+        }
 
     }
 }
